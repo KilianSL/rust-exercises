91eef58caf7a122fa646be7574fcacece9ad3fd8
Kilian SL <kilian@theaicore.com>
2023-09-20 00:51:00 +0100

    Factors out code to get user input

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,31 @@
 use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
+use std::num::ParseIntError;
+
+fn get_user_input() -> Result<u32, ParseIntError> {
+    println!("Input a guess:");
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line.");
+
+    guess.trim().parse()
+}
 
 fn main() {
     println!("Guess the number!");
-
     let secret_number = rand::thread_rng().gen_range(1..=10);
 
     loop {
-        println!("Input a guess:");
-        let mut guess = String::new();
-
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line.");
-
-        let guess: u32 = match guess.trim().parse() {
+        let guess = match get_user_input() {
             Ok(num) => num,
             Err(_) => {
-                println!("Please enter a valid number.");
+                println!("Please enter a valid number");
                 continue;
             }
         };
-
         println!("You guessed {guess}");
 
         match guess.cmp(&secret_number) {
