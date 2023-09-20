a498e984a5e12f1bcd48dcad18e4b8d23c6dca4d
Kilian SL <kilian@theaicore.com>
2023-09-21 00:07:09 +0100

    WIP implementing a deck of cards

--- blackjack/src/main.rs
+++ blackjack/src/main.rs
@@ -1,7 +1,110 @@
+use std::fmt::Display;
+
+enum Value {
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King,
+    Ace,
+}
+
+impl Value {
+    fn get_numeric_value(&self) -> u32 {
+        match self {
+            Value::Two => 2,
+            Value::Three => 3,
+            Value::Four => 4,
+            Value::Five => 5,
+            Value::Six => 6,
+            Value::Seven => 7,
+            Value::Eight => 8,
+            Value::Nine => 9,
+            Value::Ace => 11,
+            _ => 10,
+        }
+    }
+    fn get_string_value(&self) -> &str {
+        match self {
+            Value::Two => "Two",
+            Value::Three => "Three",
+            Value::Four => "Four",
+            Value::Five => "Five",
+            Value::Six => "Six",
+            Value::Seven => "Seven",
+            Value::Eight => "Eight",
+            Value::Nine => "Nine",
+            Value::Ten => "Ten",
+            Value::Jack => "Jack",
+            Value::Queen => "Queen",
+            Value::King => "King",
+            Value::Ace => "Ace",
+        }
+    }
+}
+
+enum Suit {
+    Hearts,
+    Diamonds,
+    Clubs,
+    Spades,
+}
+
+impl Suit {
+    fn get_string_value(&self) -> &str {
+        match self {
+            Suit::Hearts => "Hearts",
+            Suit::Diamonds => "Diamonds",
+            Suit::Clubs => "Clubs",
+            Suit::Spades => "Spades",
+        }
+    }
+}
+
+struct Card {
+    suit: Suit,
+    value: Value,
+}
+
+impl Card {
+    fn new(value: Value, suit: Suit) -> Card {
+        Card { suit, value }
+    }
+    fn get_string_value(&self) -> String {
+        format!(
+            "{} of {}",
+            self.value.get_string_value(),
+            self.suit.get_string_value()
+        )
+    }
+}
+
+impl Display for Card {
+    fn fmt(&self, _: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        print!("{}", self.get_string_value());
+        Ok(())
+    }
+}
+
 struct Deck {
-    cards: [String; 52],
+    cards: [Card; 52],
+}
+
+impl Deck {
+    fn new() -> Deck {
+        let mut cards = Vec::new();
+        for suit in Suit {}
+    }
 }
 
 fn main() {
-    println!("Hello");
+    let card = Card::new(Value::Ace, Suit::Spades);
+    println!("Your card is the {}", card);
 }
